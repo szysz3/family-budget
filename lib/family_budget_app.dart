89fdd8c18fc0b85de775186aa62683fdc805753d
szysz3 <michalszyszka.m@gmail.com>
2020-11-07 22:28:09 +0100

    Add bouncing scroll physics

--- lib/family_budget_app.dart
+++ lib/family_budget_app.dart
@@ -1,5 +1,6 @@
 import 'package:family_budget/page/splash_page.dart';
 import 'package:family_budget/widget/app_theme.dart';
+import 'package:family_budget/widget/custom_scroll_behavior.dart';
 import 'package:flutter/material.dart';
 
 class FamilyBudgetApp extends StatelessWidget {
@@ -7,6 +8,12 @@ class FamilyBudgetApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: CustomScrollBehavior(),
+          child: child,
+        );
+      },
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
---
+++ lib/widget/custom_scroll_behavior.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class CustomScrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return BouncingScrollPhysics();
+  }
+}
